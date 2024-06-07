b5bd5961ddd5e629e9bb45a8f89d4e361927e389
sibby <sarah.ibsen@hotmail.com>
2024-06-06 21:00:13 -0500

    brute force attack on an input username. username is then hashed to show that hashing is a good but not a good enough prevention against brute force attacks. will add more limitations later (such as incorrect password limit, maybe multi-authentication, etc.)

---
+++ fun.py
@@ -0,0 +1,11 @@
+import time
+
+
+alphabet = open('dictionary.txt').readlines()
+
+def dictionary_attack(user_password, alphabet):
+    start_time = time.time()
+    x = list(user_password)
+    print(list)
+
+    
